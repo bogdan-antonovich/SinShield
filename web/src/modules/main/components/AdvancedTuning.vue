911af9aab58a0869141bc63411a7345e9ed0df05
Bogdan <programjibogdan@gmail.com>
2026-09-16 15:18:51 +0400

    Expand mobile section buttons

--- web/src/modules/main/components/AdvancedTuning.vue
+++ web/src/modules/main/components/AdvancedTuning.vue
@@ -29,7 +29,7 @@ import BaseContainer from '@/common/components/BaseContainer.vue'
           </p>
         </div>
 
-        <BaseButton to="/products" variant="inverse" class="mt-10 ring-1 ring-navy/10">
+        <BaseButton to="/products" variant="inverse" class="mt-10 w-full sm:w-fit">
           Get Started
         </BaseButton>
       </div>
